493ca419b27d0114548ce65e22dad9813bb8322e
Romanas <go.romanas@gmail.com>
2020-08-25 13:01:52 +0300

    Added reconnecting web socket

--- frontend/src/app/page/chat/chat-page.tsx
+++ frontend/src/app/page/chat/chat-page.tsx
@@ -64,7 +64,12 @@ class ChatComponent extends React.Component<Props, State> {
         return newUrl;
     };
 
-    public ws = new ReconnectingWebSocket(this.getSocketUrl());
+    private options: {} = {
+        connectionTimeout: 1000,
+        maxRetries: 10,
+    };
+
+    public ws = new ReconnectingWebSocket(this.getSocketUrl(), this.options);
 
     public readonly state: State = {
         messages: [],
